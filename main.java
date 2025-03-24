013cdd2c8aec2be5f7accef641938a08c6838185
Alexis Le Brun <alexis.lebrun@ynov.com>
2025-03-24 10:37:20 +0100

    push_of_main_electauto

---
+++ main.java
@@ -0,0 +1,12 @@
+// main.java
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
+
+public class Carte {
+    private String nom;
+    private String prenom;
+    private String typeContrat;
+    private Float soldeForfait;
