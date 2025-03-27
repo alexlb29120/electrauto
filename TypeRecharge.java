dbdb63746d0f98d3f79908759f1edeb3b0a59f1b
Alexandre Gingast <alexandre.gingast@ynov.com>
2025-03-27 09:08:32 +0100

    add TypeRecharge.java

---
+++ TypeRecharge.java
@@ -0,0 +1,17 @@
+class TypeRecharge {
+    private String nom;
+    private int puissance;
+
+    public TypeRecharge(String nom, int puissance) {
+        this.nom = nom;
+        this.puissance = puissance;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getPuissance() {
+        return puissance;
+    }
+}
